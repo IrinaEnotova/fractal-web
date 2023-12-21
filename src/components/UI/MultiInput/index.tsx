aa5cfee0dc629ea9ef345f3d0a0780cfce32d44d
IrinaEnotova <irinkayashina@gmail.com>
2023-12-21 18:35:26 +0400

    refactor: update multiInput component

--- src/components/UI/MultiInput/index.tsx
+++ src/components/UI/MultiInput/index.tsx
@@ -1,4 +1,5 @@
 import { FormEvent, InputHTMLAttributes, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import cls from '../../../utils/classnames';
 import deleteIcon from '../../../assets/delete-icon.svg';
 import plusIcon from '../../../assets/plus-icon.svg';
@@ -8,25 +9,44 @@ import Button from '../Button';
 interface MultiInputProps extends InputHTMLAttributes<HTMLInputElement> {}
 
 export default function MultiInput({ ...props }: MultiInputProps) {
-  const [inputNumbers, seInputNumber] = useState([1, 2, 3]);
+  const [inputs, setInputs] = useState([
+    { id: '1', text: '' },
+    { id: '2', text: '' },
+    { id: '3', text: '' },
+  ]);
 
-  const deleteItem = (itemNumber: number) => {
-    seInputNumber(inputNumbers.filter((inputNumber) => inputNumber !== itemNumber));
+  const deleteItem = (itemNumber: string) => {
+    setInputs(inputs.filter((input) => input.id !== itemNumber));
   };
 
   const addItem = (event: FormEvent) => {
     event.preventDefault();
-    seInputNumber([...inputNumbers, Math.max(...inputNumbers) + 1]);
+    setInputs([...inputs, { id: uuidv4(), text: '' }]);
   };
 
   return (
     <div>
       <label className={styles.label}>
         <span>Преимущества</span>
-        {inputNumbers.map((item) => (
-          <div key={item} className={styles.inputWrapper}>
+        {inputs.map((item) => (
+          <div key={item.id} className={styles.inputWrapper}>
             <input
               {...props}
+              value={inputs.find((input) => item.id === input.id)?.text}
+              onChange={(event) => {
+                setInputs(
+                  inputs.reduce((acc: { id: string; text: string }[], input) => {
+                    if (input.id === item.id) {
+                      input.text = event.target.value;
+                      acc.push(input);
+                      return acc;
+                    } else {
+                      acc.push(input);
+                      return acc;
+                    }
+                  }, [])
+                );
+              }}
               id={`field-advatages-${item}`}
               className={cls(styles.input, 'error-input')}
               type="text"
@@ -36,7 +56,7 @@ export default function MultiInput({ ...props }: MultiInputProps) {
             <img
               id={`button-remove-${item}`}
               className={styles.deleteItem}
-              onClick={() => deleteItem(item)}
+              onClick={() => deleteItem(item.id)}
               src={deleteIcon}
               alt="Delete"
             />
